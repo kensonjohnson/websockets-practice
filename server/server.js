ddcb7c8edae3550456439904a66c7f01adb4f263
Kenson Johnson <kenson.johnson@outlook.com>
2023-04-30 10:41:22 -0500

    Added some test logic
    Properly setup the event handlers.
    
    Added some logic to close the server after 5 successful connections.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
-import { WebSocket } from "ws";
-const wss = new WebSocket("ws://Orpheus:3000");
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 3000 });
 
-wss.onmessage = function (e) {
+let numberOfRequests = 0;
+
+wss.on("connection", (ws) => {
   console.log("Connection Established.");
-  let message = JSON.parse(e.data);
-  console.log(message);
-};
+  ws.onmessage = (e) => {
+    console.log(JSON.parse(e.data));
+  };
+  console.log("Message sent.");
+  numberOfRequests++;
+  setTimeout(() => {
+    let msg = "This message came from the server.";
+    ws.send(JSON.stringify(msg));
+    // ws.close()
+  }, 500);
+});
+
+wss.on("error", console.error);
+
+wss.on("close", () => {
+  console.log("Server shutting down.");
+});
+
+const timer = setInterval(() => {
+  if (numberOfRequests > 4) {
+    wss.close();
+    clearInterval(timer);
+  }
+}, 100);
